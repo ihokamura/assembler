5eb83278048ce396e32c8e10eeea8a72cc8b8930
ihokamura <ihokamura.contact@gmail.com>
2021-01-10 09:46:26 +0900

    Improve test.

--- test/generator/test_sub.c
+++ test/generator/test_sub.c
@@ -2,53 +2,47 @@
 #include "test_common.h"
 
 
+static BinaryOperationInfo make_test_data_sub(intmax_t lhs, intmax_t rhs)
+{
+    return (BinaryOperationInfo){"sub", lhs, rhs, lhs - rhs};
+}
+
+
 static BinaryOperationInfo make_test_data_sub_reg_imm(const ImmediateInfo *imm_info)
 {
-    intmax_t lhs = imm_info->sint_max_value;
-    intmax_t rhs = 1;
-    intmax_t result = lhs - rhs;
-
-    return (BinaryOperationInfo){"sub", lhs, rhs, result};
+    return make_test_data_sub(imm_info->sint_max_value, 1);
 }
 
 
 static BinaryOperationInfo make_test_data_sub_reg_reg(const RegisterInfo *reg1_info, const RegisterInfo *reg2_info)
 {
-    intmax_t lhs = convert_size_to_sint_max_value(reg1_info->size);
-    intmax_t rhs = 1;
-    intmax_t result = (reg1_info->index == reg2_info->index) ? 0 : lhs - rhs;
-
-    return (BinaryOperationInfo){"sub", lhs, rhs, result};
+    if(reg1_info == reg2_info)
+    {
+        intmax_t operand = convert_size_to_sint_max_value(reg1_info->size);
+        return make_test_data_sub(operand, operand);
+    }
+    else
+    {
+        return make_test_data_sub(convert_size_to_sint_max_value(reg1_info->size), 1);
+    }
 }
 
 
 static BinaryOperationInfo make_test_data_sub_reg_mem(const RegisterInfo *reg_info)
 {
-    intmax_t lhs = convert_size_to_sint_max_value(reg_info->size);
-    intmax_t rhs = 1;
-    intmax_t result = lhs - rhs;
-
-    return (BinaryOperationInfo){"sub", lhs, rhs, result};
+    return make_test_data_sub(convert_size_to_sint_max_value(reg_info->size), 1);
 }
 
 
 static BinaryOperationInfo make_test_data_sub_mem_imm(const ImmediateInfo *imm_info)
 {
-    intmax_t lhs = imm_info->sint_max_value;
-    intmax_t rhs = 1;
-    intmax_t result = lhs - rhs;
-
-    return (BinaryOperationInfo){"sub", lhs, rhs, result};
+    return make_test_data_sub(imm_info->sint_max_value, 1);
 }
 
 
 static BinaryOperationInfo make_test_data_sub_mem_reg(const RegisterInfo *reg_info)
 {
-    intmax_t lhs = convert_size_to_sint_max_value(reg_info->size);
-    intmax_t rhs = 1;
-    intmax_t result = lhs - rhs;
-
-    return (BinaryOperationInfo){"sub", lhs, rhs, result};
+    return make_test_data_sub(convert_size_to_sint_max_value(reg_info->size), 1);
 }
 
 
